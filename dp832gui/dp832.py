14a50dd07c2fe494ddcdcb60938f770eaf78f072
Paul Stenius <stenius@gmail.com>
2016-06-09 12:45:01 -0500

    use network port instead of usb

--- dp832gui/dp832.py
+++ dp832gui/dp832.py
@@ -1,38 +1,35 @@
-
-#Install pyvisa, use easy_install for example:
-#easy_install pyvisa
-import visa
+import vxi11
 
 class DP832(object):
     def __init__(self):
         pass
 
-    def conn(self, constr="USB0::0x1AB1::0x0E11::DPXXXXXXXXXXX::INSTR"):
+    def conn(self, address="10.8.10.104"):
         """Attempt to connect to instrument"""
-        rm = visa.ResourceManager()
-        self.inst = rm.open_resource(constr)
+        self.instra = vxi11.Instrument(address)
 
     def identify(self):
         """Return identify string which has serial number"""
-        return self.inst.query("*IDN?")
+        return self.instra.ask("*IDN?")
 
     def readings(self, channel="CH1"):
         """Read voltage/current/power from CH1/CH2/CH3"""       
-        resp = self.inst.query("MEAS:ALL? %s"%channel)
+        resp = self.instra.ask("MEAS:ALL? %s"%channel)
         resp = resp.split(',')
         dr = {"v":float(resp[0]), "i":float(resp[1]), "p":float(resp[2])}
         return dr
 
     def dis(self):
-        del self.inst
+        del self.instra
 
     def writing(self, command=""):
-        self.inst.write(command)
+        print 'not implemented'
+        #self.inst.write(command)
         
 if __name__ == '__main__':
     test = DP832()
 
-    #Insert your serial number here / confirm via Ultra Sigma GUI
-    test.conn("USB0::0x1AB1::0x0E11::DPXXXXXXXXXXX::INSTR")
+    # Insert your scopes address here
+    test.conn()
     
     print test.readings()
